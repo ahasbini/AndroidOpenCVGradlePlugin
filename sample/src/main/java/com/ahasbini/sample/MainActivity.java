f56c8612e94a9f44e697e6b22d64a7d71e5b68d9
Michal Hajek <michalhajek@centrum.cz>
2020-02-19 00:10:45 +0100

    Added sample Android application

---
+++ sample/src/main/java/com/ahasbini/sample/MainActivity.java
@@ -0,0 +1,87 @@
+package com.ahasbini.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+/**
+ * Created by mikrop on 16-Feb-20.
+ */
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    public static final String TAG = "MainActivity";
+
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(TAG, "OpenCV failed to load!");
+        } else {
+            Log.i(TAG, "OpenCV loaded successfully");
+        }
+    }
+
+    private JavaCameraView mCameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        mCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
+        mCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.i(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initiation");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
+        } else {
+            Log.i(TAG, "OpenCV library found inside package. Using it");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCameraView != null)
+            mCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.gray();
+    }
+}
